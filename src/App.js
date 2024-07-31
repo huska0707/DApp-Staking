fdd34c623ac22a4e0dfa46664ed2b5decad25cda
huska0707 <hussaink001haled@gmail.com>
2024-07-31 07:57:58 -0500

    updateHappyBalance and populate

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import CardDeck from "react-bootstrap/CardDeck";
-import HappyChefContract from './contracts/HappyChef.json';
+import HappyChefContract from "./contracts/HappyChef.json";
 
 import getWeb3 from "./getWeb3";
+import Pool from "./components/Pool";
 
 const App = () => {
   const [web3, setWeb3] = useState(null);
@@ -14,24 +15,35 @@ const App = () => {
   const [happyBalance, setHappyBalance] = useState(0);
   const [accounts, setAccounts] = useState(null);
   const [contract, setContract] = useState(null);
+  const [pools, setPools] = useState([]);
+
+  let happy;
 
   useEffect(() => {
     const init = async () => {
-      const web3Instance = await getWeb3();
+      try {
+        const web3Instance = await getWeb3();
 
-      const userAccount = await web3Instance.eth.getAccounts();
+        const userAccount = await web3Instance.eth.getAccounts();
 
-      const networkId = await web3Instance.eth.net.getId();
-      const deployNetwork = HappyChefContract.networks[networkId];
+        const networkId = await web3Instance.eth.net.getId();
+        const deployNetwork = HappyChefContract.networks[networkId];
 
-      const contractInstance = new web3Instance.eth.Contract(
-        HappyChefContract.abi,
-        deployNetwork && deployNetwork.address
-      )
+        const contractInstance = new web3Instance.eth.Contract(
+          HappyChefContract.abi,
+          deployNetwork && deployNetwork.address
+        );
 
-      setWeb3(web3Instance);
-      setAccounts(userAccount);
-      setContract(contractInstance);
+        setWeb3(web3Instance);
+        setAccounts(userAccount);
+        setContract(contractInstance);
+        populate(contractInstance, userAccount[0]);
+      } catch (error) {
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`
+        );
+        console.error(error);
+      }
     };
 
     init();
@@ -45,8 +57,31 @@ const App = () => {
         .then((res) => setHappyPrice(web3.utils.fromWei(res, "ether")));
     }
   };
+
   const ellipsis = (str) => {
     return str ? str.slice(0, 6) + "..." + str.slice(str.length - 4) : "";
+  };
+
+  const updateHappyBalance = () => {
+    if (happy && accounts) {
+      happy.methods
+        .balanceOf(accounts[0])
+        .call()
+        .then((balance) => {
+          setHappyBalance(
+            Number(web3.utils.fromWei(balance, "ether")).toFixed(6)
+          );
+        });
+    }
+  };
+
+  const populate = async (contractInstance, account) => {
+    updateHappyBalance();
+    updatedHappyPrice();
+
+    const nbPools = await contractInstance.methods.getnbPools().call();
+    const poolsArray = Array.from({ length: nbPools }, (_, i) => i);
+    setPools(poolsArray);
   };
 
   if (!web3) {
@@ -77,7 +112,18 @@ const App = () => {
       </Navbar>
 
       <Container>
-        <CardDeck></CardDeck>
+        <CardDeck>
+          {pools.map((pool) => {
+            <Pool
+              web3={web3}
+              contract={contract}
+              account={accounts[0]}
+              updateHappyBalance={updateHappyBalance}
+              id={pool}
+              key={pool}
+            />;
+          })}
+        </CardDeck>
       </Container>
     </>
   );
